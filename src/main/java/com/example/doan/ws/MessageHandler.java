83d76bcca06fabdc1845ad4b64d70a9de680bc6a
thanhk99 <thanh040804@gmail.com>
2025-03-25 20:42:37 +0700

    prepare b2

---
+++ src/main/java/com/example/doan/ws/MessageHandler.java
@@ -0,0 +1,28 @@
+package com.example.doan.ws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MessageHandler extends TextWebSocketHandler {
+    
+    private List<WebSocketSession> sessions= new ArrayList<>();
+
+    @Override 
+    public void afterConnectionEstablished( WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+    @Override 
+    public void afterConnectionClosed(WebSocketSession session,  CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    @Override 
+    public void handleMessage(WebSocketSession session,  WebSocketMessage<?> message) throws Exception {
+        
+    }
+}
