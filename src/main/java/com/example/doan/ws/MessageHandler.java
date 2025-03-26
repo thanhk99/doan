1e0419600595626b964fd37aa96a7c60a0f27ea7
thanhk99 <thanh040804@gmail.com>
2025-03-26 08:55:19 +0700

    prepare b2 v3

--- src/main/java/com/example/doan/ws/MessageHandler.java
+++ src/main/java/com/example/doan/ws/MessageHandler.java
@@ -1,7 +1,9 @@
 package com.example.doan.ws;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
@@ -11,11 +13,13 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class MessageHandler extends TextWebSocketHandler {
     
     private List<WebSocketSession> sessions= new ArrayList<>();
+    private Map <String, WebSocketSession> userSession = new HashMap<>();
 
     @Override 
     public void afterConnectionEstablished( WebSocketSession session) throws Exception {
         sessions.add(session);
         String username = (String) session.getAttributes().get("username");
+        userSession.put(username, session);
     }
     @Override 
     public void afterConnectionClosed(WebSocketSession session,  CloseStatus status) throws Exception {
